716414d5f67cc566d5d07d8e166474e4a7dbb5bb
miyoalpizar <Miyo@DESKTOP-VOHF5HE>
2021-06-30 10:03:47 -0500

    Add project files.

---
+++ WebMailing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebMailing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
