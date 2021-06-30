75b8664118765a36c816606e484f119d4dbc7eb5
miyoalpizar <Miyo@DESKTOP-VOHF5HE>
2021-06-30 10:12:40 -0500

    Models Done

---
+++ WebMailing.Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebMailing.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "E-mail")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Enter a valid E-mail")]
+        public string Email { get; set; }
+    }
+}
